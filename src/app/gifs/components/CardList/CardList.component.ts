8d1fff83097ec5bb221f7cbbb4677b2291707f3c
stephvega <136370358+stephvega@users.noreply.github.com>
2024-12-12 17:49:33 -0600

    add angular

---
+++ src/app/gifs/components/CardList/CardList.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-card-list',
+  templateUrl: './card-list.component.html'
+})
+export class CardListComponent { }
+
---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar:</h5>
+    <input type="text"
+    class="form-control"
+    placeholder="Buscar gifs..."
+    >
+  `
+})
+
+export class SearchBoxComponent {
+  constructor() { }
+
+searchTag(newTag: String) {
+  console.log({newTag})
+}
+}
